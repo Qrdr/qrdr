511bf9be55b0cb49b42ad144c3871aa790e25770
RemusW <rwong@Iteration110.localdomain>
2020-02-03 16:53:59 -0600

    cleared directories

--- qrdr/src/app/directives/hide-fab.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HideFabDirective } from './hide-fab.directive';
-
-describe('HideFabDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HideFabDirective();
-    expect(directive).toBeTruthy();
-  });
-});
