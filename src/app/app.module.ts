4d06ad9353b70809b3fc2f948f935aae597e6480
Derek Wu <derekwu35@gmail.com>
2020-03-27 03:39:47 -0500

    adjusted padding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -13,7 +15,6 @@ import { AppComponent } from './app.component';
 import { CartPageModule } from './pages/cart/cart.module';
 import { ItemDetailPageModule } from './pages/item-detail/item-detail.module';
 import { CartService } from './services/cart.service';
-<<<<<<< HEAD
 import { MenuService } from './services/menu.service';
 
 @NgModule({
@@ -26,6 +27,8 @@ import { MenuService } from './services/menu.service';
 		AppRoutingModule,
 		CartPageModule,
 		ItemDetailPageModule,
+		AngularFireModule.initializeApp(environment.firebase),
+		AngularFirestoreModule,
 		ServiceWorkerModule.register('ngsw-worker.js', {
 			enabled: environment.production
 		})
@@ -38,30 +41,5 @@ import { MenuService } from './services/menu.service';
 		{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
 	],
 	bootstrap: [AppComponent]
-=======
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    IonicModule.forRoot(),
-    IonicStorageModule.forRoot(),
-    AppRoutingModule,
-    CartPageModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    CartService,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
->>>>>>> b4effcafc77917a1a19791bc5280a1bae0bfb7d1
 })
 export class AppModule {}
